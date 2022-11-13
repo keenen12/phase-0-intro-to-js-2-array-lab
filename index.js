80fda95f450fae64e33e17a3cc46da8fe875c7b7
Keenen Harral <kharral23@yahoo.com>
2022-11-12 20:15:18 -0800

    commit tree d7322f4ebc1ecb596f868b657f89b07c3bd24025

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, 'Broom'];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats2 = ['Arnold', ...cats];
+    return newCats2;
+}
+
+function removeLastCat() {
+    const newCats3 = cats.slice(0, -1);
+    return newCats3;
+}
+
+function removeFirstCat() {
+    const newCats4 = cats.slice(1);
+    return newCats4;
+}
